c18bd3896d213af4acad6eeeb77700694ba97bce
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-09 11:56:43 +0100

    first commit after spring boot app created using start.spring.io

---
+++ src/main/java/com/gmailatgauru/dogra/exceltoexcelparsingwebapp/ExcelToExcelParsingWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.gmailatgauru.dogra.exceltoexcelparsingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelToExcelParsingWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelToExcelParsingWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmailatgauru/dogra/exceltoexcelparsingwebapp/ExcelToExcelParsingWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmailatgauru.dogra.exceltoexcelparsingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelToExcelParsingWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
