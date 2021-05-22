17ed4e62699b453ce920a216091f89452d2b83f0
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-22 21:40:18 +0100

    ReadXlsxTest.java Record.java is TransactionRecord.java

--- src/test/java/gmailgdogra/ReadXlsxTest.java
+++ src/test/java/gmailgdogra/ReadXlsxTest.java
@@ -12,14 +12,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReadXlsxTest {
 
-    private List<Record> records;
+    private List<TransactionRecord> transactionRecords;
 
-    private final Record FIRST_RECORD = new Record("Abdul",
+    private final TransactionRecord FIRST_RECORD = new TransactionRecord("Abdul",
             "Khan",
             LocalDateTime.of(2021, 5, 8, 5, 43, 35),
             "PLA0102 - Turnstile West IN");
 
-    private final Record LAST_RECORD = new Record("Tahiru",
+    private final TransactionRecord LAST_RECORD = new TransactionRecord("Tahiru",
             "Haruna",
             LocalDateTime.of(2021, 5, 9, 5, 39, 58),
             "Thames LSI0901 - Turnstile South IN");
@@ -27,21 +27,21 @@ class ReadXlsxTest {
     @BeforeEach
     void fillData() throws IOException {
         String TEST_FILE_PATH = "src/main/resources/testFile.xlsx";
-        records = ReadXlsx.parse(TEST_FILE_PATH);
+        transactionRecords = ReadXlsx.parse(TEST_FILE_PATH);
     }
 
     @Test
     @DisplayName("checking total number of records")
     void parseTotalRowsCheck() {
-        assertEquals(103, records.size());
+        assertEquals(103, transactionRecords.size());
     }
 
     @Test
     @DisplayName("checking first and last record")
     void parseFirstRowCheck() {
         assertAll("records",
-                () -> assertEquals(FIRST_RECORD, records.get(0)),
-                () -> assertEquals(LAST_RECORD, records.get(records.size() - 1))
+                () -> assertEquals(FIRST_RECORD, transactionRecords.get(0)),
+                () -> assertEquals(LAST_RECORD, transactionRecords.get(transactionRecords.size() - 1))
         );
     }
 }
