980d2416eb25ca0cfb6d4350ba082b2bb55cba71
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-23 05:38:26 +0100

    UserInput.getOfficersLocationFromUser() to UserInput.getOfficersShiftInfo(), getOfficersShiftInfo() calls  two new methods getLocation() & getShiftType()

--- src/main/java/gmailgdogra/UserInput.java
+++ src/main/java/gmailgdogra/UserInput.java
@@ -6,19 +6,32 @@ import java.util.Set;
 
 public class UserInput {
 
-    public static Set<Officer> getOfficersLocationFromUser(List<TransactionRecord> transactionRecords) {
+    public static Set<Officer> getOfficersShiftInfo(List<TransactionRecord> transactionRecords) {
 
-        Scanner scanner = new Scanner(System.in);
         Set<Officer> officers = ExtractOfficers.from(transactionRecords);
-
         for (Officer officer : officers) {
             printAllLocations();
-            System.out.print("\nOfficer: " + officer.getFullName() + ", Enter Location(id): ");
-            String locationAsNumber = scanner.nextLine().trim();
-            Location location = convertInputToLocation(locationAsNumber);
+            Location location = getLocation(officer);
+            boolean isDayShift = getShiftType();
             officer.setLocation(location);
+            officer.setDayShift(isDayShift);
         }
         return officers;
+    }
+
+    private static Location getLocation(Officer officer) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nOfficer: " + officer.getFullName() + ", Enter Location(id): ");
+        String locationAsNumber = scanner.nextLine().trim();
+        return convertInputToLocation(locationAsNumber);
+    }
+
+    private static boolean getShiftType() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Is it day shift(y/n): ");
+        String isDayShift = scanner.nextLine().trim().toLowerCase();
+        if (isDayShift.equals("")) { return false; }
+        return isDayShift.charAt(0) == 'y';
     }
 
     private static Location convertInputToLocation(String location) {
@@ -37,6 +50,7 @@ public class UserInput {
     }
 
     private static void printAllLocations() {
+        System.out.println("===============================================================");
         System.out.println("Choose a number from below or press enter to ignore the officer");
         System.out.print("Main Gate(1), ");
         System.out.print("Visitors Reception(2), ");
