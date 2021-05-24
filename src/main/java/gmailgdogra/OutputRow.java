1c7498356b23234239ade2c8a4215e7ffaccf462
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-24 23:52:53 +0100

    created OutputRow.java, it's 4 fields is 4 columns of each row

---
+++ src/main/java/gmailgdogra/OutputRow.java
@@ -0,0 +1,35 @@
+package gmailgdogra;
+
+public class OutputRow {
+    private final String firstName;
+    private final String lastName;
+    private final String swipeTime;
+    private final String deviceName;
+
+    public static OutputRow of(String firstName, String lastName, String swipeTime, String deviceName) {
+        return new OutputRow(firstName, lastName, swipeTime, deviceName);
+    }
+
+    public OutputRow(String firstName, String lastName, String swipeTime, String deviceName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.swipeTime = swipeTime;
+        this.deviceName = deviceName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSwipeTime() {
+        return swipeTime;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+}
