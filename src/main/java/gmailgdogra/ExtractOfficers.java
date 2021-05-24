f30752965b702a568276346b7ac77c67ba326bf9
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-24 01:22:10 +0100

    ExtracOfficer.java modified bc TransactionRecord.java renamed to SwipeRecord.java

--- src/main/java/gmailgdogra/ExtractOfficers.java
+++ src/main/java/gmailgdogra/ExtractOfficers.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 
 public class ExtractOfficers {
 
-    public static Set<Officer> from(List<TransactionRecord> transactionRecords) {
-        return transactionRecords.stream()
+    public static Set<Officer> from(List<SwipeRecord> swipeRecords) {
+        return swipeRecords.stream()
                 .map(transactionRecord -> new Officer(transactionRecord.getFirstName(),
                         transactionRecord.getLastName()))
                 .collect(Collectors.toSet());
