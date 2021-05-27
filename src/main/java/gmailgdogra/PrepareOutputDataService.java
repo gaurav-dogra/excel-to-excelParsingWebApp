1ce7070a2ecd8afaaae8a8ab1ee104659063447c
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-27 03:23:50 +0100

    PrepareOutputDataService.java, is @Service, hasExcelFormat(MultipartFile file) added, ReadXlsx.parse() takes InputStream

--- src/main/java/gmailgdogra/PrepareOutputDataService.java
+++ src/main/java/gmailgdogra/PrepareOutputDataService.java
@@ -1,15 +1,22 @@
 package gmailgdogra;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class PrepareOutputData {
+@Service
+public class PrepareOutputDataService {
 
-    public static List<SwipeRecord> from(String filePathAndName) throws IOException {
+    public static String EXCEL_FILE_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+    public static List<SwipeRecord> from(InputStream inputStream) throws IOException {
         List<SwipeRecord> outputData = new ArrayList<>();
-        List<SwipeRecord> swipeRecords = ReadXlsx.parse(filePathAndName);
+        List<SwipeRecord> swipeRecords = ReadXlsx.parse(inputStream);
         Set<Officer> officers = UserInput.getOfficers(swipeRecords);
         SwipeProcessor swipeProcessor = new SwipeProcessor(swipeRecords);
         for (Officer officer : officers) {
@@ -19,6 +26,10 @@ public class PrepareOutputData {
             }
         }
         return outputData;
+    }
+
+    public static boolean hasExcelFormat(MultipartFile file) {
+        return EXCEL_FILE_TYPE.equals(file.getContentType());
     }
 }
 
