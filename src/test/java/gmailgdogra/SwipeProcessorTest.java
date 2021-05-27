9a635620a29cadc2e118ae01dfff4fc4eb203652
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-27 03:32:38 +0100

    Because ReadXlsx.parse() now takes InputStream, changes to create a InputStream from fileName

--- src/test/java/gmailgdogra/SwipeProcessorTest.java
+++ src/test/java/gmailgdogra/SwipeProcessorTest.java
@@ -3,8 +3,13 @@ package gmailgdogra;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,7 +22,18 @@ class SwipeProcessorTest {
 
     @BeforeAll
     static void setUp() throws IOException {
-        List<SwipeRecord> swipeRecords = ReadXlsx.parse("src/main/resources/testFile.xlsx");
+        Path path = Paths.get("src/main/resources/testFile.xlsx");
+        String name = "testFile.xlsx";
+        String originalFileName = "testFile.xlsx";
+        String contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        byte[] content = null;
+        try {
+            content = Files.readAllBytes(path);
+        } catch (final IOException ignored) {
+        }
+        MultipartFile result = new MockMultipartFile(name,
+                originalFileName, contentType, content);
+        List<SwipeRecord> swipeRecords = ReadXlsx.parse(result.getInputStream());
         abdulKhan = new Officer("Abdul", "Khan", Location.TL_PLAISTOW, true);
         basharatIqbal = new Officer("Basharat", "Iqbal", Location.MAIN_GATE, false);
         swipeProcessor = new SwipeProcessor(swipeRecords);
