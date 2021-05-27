031bea0585f4ce8470eda29331a42ff0a1b34277
Gaurav Dogra <gauru.dogra@gmail.com>
2021-05-27 03:21:24 +0100

    removed temp code, functionality is moved to RestController

--- src/main/java/gmailgdogra/ExcelToExcelParsingWebApp.java
+++ src/main/java/gmailgdogra/ExcelToExcelParsingWebApp.java
@@ -11,12 +11,5 @@ public class ExcelToExcelParsingWebApp {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(ExcelToExcelParsingWebApp.class, args);
-        List<SwipeRecord> outputData =
-                PrepareOutputData.from("src/main/resources/testFile.xlsx");
-        boolean isSuccess = WriteOutputXlsx.write(outputData);
-        if (isSuccess) {
-            System.out.println("File Created and Saved.");
-        }
     }
-
 }
