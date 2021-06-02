4d878f97c264e00e2352671310d6e7a9c376fe94
Gaurav Dogra <gauru.dogra@gmail.com>
2021-06-02 02:18:46 +0100

    ExtractOfficers.java, change of name of variable to swipeRecord

--- src/main/java/gmailgdogra/ExtractOfficers.java
+++ src/main/java/gmailgdogra/ExtractOfficers.java
@@ -8,8 +8,8 @@ public class ExtractOfficers {
 
     public static Set<Officer> from(List<SwipeRecord> swipeRecords) {
         return swipeRecords.stream()
-                .map(transactionRecord -> new Officer(transactionRecord.getFirstName(),
-                        transactionRecord.getLastName()))
+                .map(swipeRecord -> new Officer(swipeRecord.getFirstName(),
+                        swipeRecord.getLastName()))
                 .collect(Collectors.toSet());
     }
 }
