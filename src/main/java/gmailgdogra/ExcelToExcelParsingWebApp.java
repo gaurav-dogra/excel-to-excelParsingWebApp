8f9eaac4276665f62d8beb48a89f0d5a350a47b4
Gaurav Dogra <gauru.dogra@gmail.com>
2021-06-04 22:26:14 +0100

    ExcelToExcelParsingWebApp.java, temp commands to see the result in console

--- src/main/java/gmailgdogra/ExcelToExcelParsingWebApp.java
+++ src/main/java/gmailgdogra/ExcelToExcelParsingWebApp.java
@@ -3,10 +3,21 @@ package gmailgdogra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
 @SpringBootApplication
 public class ExcelToExcelParsingWebApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(ExcelToExcelParsingWebApp.class, args);
+        File file = new File("src/main/resources/testFile.xlsx");
+        FileInputStream inputStream = new FileInputStream(file);
+        List<SwipeRecord> allSwipes = ReadXlsxService.parse(inputStream);
+        List<SwipeRecord> outputData = SwipeProcessor.getOutputDataFrom(allSwipes);
+        outputData.forEach(System.out::println);
+        WriteOutputXlsx.write(outputData);
     }
 }
