6aac9c701fa5b1579b808ff31b47667bd0242de4
Gaurav Dogra <gauru.dogra@gmail.com>
2021-06-04 05:36:49 +0100

    SwipeProcessor.java, wrote the business logic from scratch

--- src/main/java/gmailgdogra/SwipeProcessor.java
+++ src/main/java/gmailgdogra/SwipeProcessor.java
@@ -1,84 +1,143 @@
 package gmailgdogra;
 
-import java.util.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SwipeProcessor {
 
-    private static final Map<Location, List<String>> allSwipeInDevices = new HashMap<>();
-    private static final Map<Location, List<String>> allSwipeOutDevices = new HashMap<>();
+    private static final List<String> allSwipeInDevices = new ArrayList<>();
+    private static final List<String> allSwipeOutDevices = new ArrayList<>();
+    private static LocalDate firstDay;
 
     static {
-        allSwipeInDevices.put(Location.MAIN_GATE,
-                List.of("Thames LSI0903 - Turnstile North IN",
-                        "Thames LSI0901 - Turnstile South IN"));
-        allSwipeInDevices.put(Location.VISITORS_RECEPTION,
-                List.of("Thames LSI0303 - Empl East Turnstile IN",
-                        "Thames LSI0301 - Empl West Turnstile IN"));
-        allSwipeInDevices.put(Location.EP_WEIGHBRIDGE,
-                List.of("Thames LSI0701 - Weighbridge IN"));
-        allSwipeInDevices.put(Location.TL_PLAISTOW,
-                List.of("PLA0102 - Turnstile West IN",
-                        "PLA0104 - Turnstile East IN"));
+        allSwipeInDevices.add("Thames LSI0903 - Turnstile North IN");
+        allSwipeInDevices.add("Thames LSI0901 - Turnstile South IN");
+        allSwipeInDevices.add("Thames LSI0303 - Empl East Turnstile IN");
+        allSwipeInDevices.add("Thames LSI0301 - Empl West Turnstile IN");
+        allSwipeInDevices.add("Thames LSI0701 - Weighbridge IN");
+        allSwipeInDevices.add("PLA0102 - Turnstile West IN");
+        allSwipeInDevices.add("PLA0104 - Turnstile East IN");
 
-        allSwipeOutDevices.put(Location.MAIN_GATE,
-                List.of("Thames LSI0904 - Turnstile North OUT",
-                        "Thames LSI0902 - Turnstile South OUT"));
-        allSwipeOutDevices.put(Location.VISITORS_RECEPTION,
-                List.of("Thames LSI0302 - Empl W Turnstile OUT",
-                        "Thames LSI0304 - Empl E Turnstile OUT"));
-        allSwipeOutDevices.put(Location.EP_WEIGHBRIDGE,
-                List.of("Thames LSI0702 - Weighbridge OUT"));
-        allSwipeOutDevices.put(Location.TL_PLAISTOW,
-                List.of("PLA0103 - Turnstile West OUT",
-                        "PLA0105 - Turnstile East OUT"));
+        allSwipeOutDevices.add("Thames LSI0904 - Turnstile North OUT");
+        allSwipeOutDevices.add("Thames LSI0902 - Turnstile South OUT");
+        allSwipeOutDevices.add("Thames LSI0302 - Empl W Turnstile OUT");
+        allSwipeOutDevices.add("Thames LSI0304 - Empl E Turnstile OUT");
+        allSwipeOutDevices.add("Thames LSI0702 - Weighbridge OUT");
+        allSwipeOutDevices.add("PLA0103 - Turnstile West OUT");
+        allSwipeOutDevices.add("PLA0105 - Turnstile East OUT");
 
     }
 
     public static List<SwipeRecord> getOutputDataFrom(List<SwipeRecord> allSwipes) {
 
         List<SwipeRecord> outputData = new ArrayList<>();
-        Set<Officer> officers = UserInput.getOfficers(allSwipes);
+        List<SwipeRecord> allInOutSwipes = allSwipes.stream()
+                .filter(swipe -> allSwipeInDevices.contains(swipe.getDeviceName()) ||
+                        allSwipeOutDevices.contains(swipe.getDeviceName()))
+                .collect(Collectors.toList());
+        Set<Officer> officers = ExtractOfficers.from(allInOutSwipes);
+        firstDay = getFirstDay(allInOutSwipes);
+
         for (Officer officer : officers) {
-            if (officer.getLocation() != null) {
-                outputData.add(getFirstSwipeIn(officer, allSwipes)); // swipe in row
-                outputData.add(getLastSwipeOut(officer, allSwipes)); // swipe out row
-            }
+
+            SwipeRecord swipeIn = getSwipeIn(allInOutSwipes, officer);
+            System.out.println("swipeIn = " + swipeIn);
+            SwipeRecord swipeOut = getSwipeOut(allInOutSwipes, officer);
+            System.out.println("swipeOut = " + swipeOut);
         }
         return outputData;
     }
 
-    public static SwipeRecord getFirstSwipeIn(Officer officer, List<SwipeRecord> allSwipes) {
-        Location loc = officer.getLocation();
-        return allSwipes.stream()
-                .filter(swipe -> officer.getFullName().equals(swipe.getFullName()))
-                .filter(swipe -> allSwipeInDevices.get(loc).contains(swipe.getDeviceName()))
+    private static SwipeRecord getSwipeOut(List<SwipeRecord> allInOutSwipes, Officer officer) {
+
+        Stream<SwipeRecord> recordsStream = allInOutSwipes.stream()
+                .filter(swipe -> swipe.getFullName().equals(officer.getFullName()))
+                .filter(swipe -> allSwipeOutDevices.contains(swipe.getDeviceName()))
                 .filter(swipe -> {
-                    if (officer.isDayShift()) {
-                        return swipe.getSwipeTime().getHour() <= 12; // touch-in is before 12:59 afternoon
+                    if (swipe.getSwipeDateTime().toLocalDate().equals(firstDay)) {
+                        return swipe.getSwipeDateTime().toLocalTime().getHour() >= 17;
                     } else {
-                        return swipe.getSwipeTime().getHour() > 12; // touch-in on or after 13:00
+                        return true;
                     }
-                })
-                .sorted()
-                .findFirst()
-                .orElseGet(() -> new SwipeRecord(officer.getFirstName(), officer.getLastName(), null, null));
+                });
+
+        if (isNightShift(allInOutSwipes, officer)) {
+            return recordsStream
+                    .filter(swipe -> swipe.getSwipeDateTime().getHour() >= 5 &&
+                            swipe.getSwipeDateTime().getHour() <= 9)
+                    .sorted()
+                    .reduce((first, second) -> second)
+                    .orElseGet(() -> new SwipeRecord(officer.getFirstName(), officer.getLastName(), null, null));
+        } else {
+            return recordsStream
+                    .filter(swipe -> swipe.getSwipeDateTime().getHour() >= 17 &&
+                            swipe.getSwipeDateTime().getHour() <= 21)
+                    .sorted()
+                    .reduce((first, second) -> second)
+                    .orElseGet(() -> new SwipeRecord(officer.getFirstName(), officer.getLastName(), null, null));
+        }
     }
 
-    public static SwipeRecord getLastSwipeOut(Officer officer, List<SwipeRecord> allSwipes) {
-        Location loc = officer.getLocation();
+    private static SwipeRecord getSwipeIn(List<SwipeRecord> allInOutSwipes, Officer officer) {
+
+        Stream<SwipeRecord> recordsStream = allInOutSwipes.stream()
+                .filter(swipe -> swipe.getFullName().equals(officer.getFullName()))
+                .filter(swipe -> allSwipeInDevices.contains(swipe.getDeviceName()))
+                .filter(swipe -> swipe.getSwipeDateTime().toLocalDate().isEqual(firstDay));
+        System.out.println(officer + ": " + isNightShift(allInOutSwipes, officer));
+
+        if (isNightShift(allInOutSwipes, officer)) {
+            return recordsStream
+                    .filter(swipe -> swipe.getSwipeDateTime().getHour() >= 17 &&
+                            swipe.getSwipeDateTime().getHour() <= 21)
+                    .sorted()
+                    .findFirst()
+                    .orElseGet(() -> new SwipeRecord(officer.getFirstName(), officer.getLastName(), null, null));
+        } else {
+            return recordsStream
+                    .filter(swipe -> swipe.getSwipeDateTime().getHour() >= 5 &&
+                            swipe.getSwipeDateTime().getHour() <= 9)
+                    .sorted()
+                    .findFirst()
+                    .orElseGet(() -> new SwipeRecord(officer.getFirstName(), officer.getLastName(), null, null));
+        }
+    }
+
+    private static boolean isNightShift(List<SwipeRecord> allInOutSwipes, Officer officer) {
+
+        if (getStreamOfSwipesForGivenOfficer(allInOutSwipes, officer)
+                .anyMatch(swipe -> swipe.getSwipeDateTime().getHour() > 21)) {
+            return true;
+        }
+
+        if (getStreamOfSwipesForGivenOfficer(allInOutSwipes, officer)
+                .anyMatch(swipe -> swipe.getSwipeDateTime().getHour() > 9 &&
+                        swipe.getSwipeDateTime().getHour() < 17)) {
+            return false;
+        }
+
+        return getStreamOfSwipesForGivenOfficer(allInOutSwipes, officer)
+                .filter(swipe -> allSwipeOutDevices.contains(swipe.getDeviceName()))
+                .anyMatch(swipe -> swipe.getSwipeDateTime().getHour() >= 5 &&
+                        swipe.getSwipeDateTime().getHour() <= 9);
+    }
+
+    private static Stream<SwipeRecord> getStreamOfSwipesForGivenOfficer(List<SwipeRecord> allInOutSwipes, Officer officer) {
+        return allInOutSwipes.stream()
+                .filter(swipe -> swipe.getFullName().equals(officer.getFullName()));
+    }
+
+    private static LocalDate getFirstDay(List<SwipeRecord> allSwipes) {
         return allSwipes.stream()
-                .filter(swipe -> officer.getFullName().equals(swipe.getFullName()))
-                .filter(swipe -> allSwipeOutDevices.get(loc).contains(swipe.getDeviceName()))
-                .filter(swipe -> {
-                    if (officer.isDayShift()) {
-                        return swipe.getSwipeTime().getHour() >= 12; // touch out must be 12 noon or after
-                    } else {
-                        return swipe.getSwipeTime().getHour() < 12; // touch out must be before 12 noon
-                    }
-                })
+                .map(swipeRecord -> swipeRecord.getSwipeDateTime().toLocalDate())
+                .distinct()
                 .sorted()
-                .reduce((first, second) -> second)
-                .orElseGet(() -> new SwipeRecord(officer.getFirstName(), officer.getLastName(), null, null));
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Failure to read the data"));
     }
 
 }
