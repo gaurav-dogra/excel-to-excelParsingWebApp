e2e0a4499f4cb06f0adef7909a8c6fa8a176b803
Gaurav Dogra <gauru.dogra@gmail.com>
2021-06-09 03:18:39 +0100

    ExcelToExcelParsingWebApp.java code for checking the program is no longer needed

--- src/main/java/gmailgdogra/ExcelToExcelParsingWebApp.java
+++ src/main/java/gmailgdogra/ExcelToExcelParsingWebApp.java
@@ -3,21 +3,10 @@ package gmailgdogra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.List;
-
 @SpringBootApplication
 public class ExcelToExcelParsingWebApp {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(ExcelToExcelParsingWebApp.class, args);
-        File file = new File("src/main/resources/testFile.xlsx");
-        FileInputStream inputStream = new FileInputStream(file);
-        List<SwipeRecord> allSwipes = ReadXlsxService.parse(inputStream);
-        List<SwipeRecord> outputData = SwipeProcessor.getOutputDataFrom(allSwipes);
-        outputData.forEach(System.out::println);
-        WriteOutputXlsx.write(outputData);
     }
 }
