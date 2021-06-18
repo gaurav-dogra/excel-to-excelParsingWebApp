c7d3fbbf27b29f2a8139b8f048afad174e6444e5
Gaurav Dogra <gauru.dogra@gmail.com>
2021-06-18 21:02:51 +0100

    created fileUploadView, UploadFailureView

--- src/main/java/gmailgdogra/MyController.java
+++ src/main/java/gmailgdogra/MyController.java
@@ -5,10 +5,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
@@ -16,35 +17,29 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Set;
 
-@RestController
-public class Controller {
+@Controller
+public class MyController {
 
     private List<SwipeRecord> allSwipes;
 
-    @GetMapping("/")
-    public String home() {
-        return "GD Welcomes you to Excel -> Excel Parsing Web App";
+    @RequestMapping("/")
+    public String uploadPage() {
+        return "uploadView";
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<ResponseMessage> upload(@RequestParam MultipartFile file) {
+    public String upload(@RequestParam("file") MultipartFile file) {
         System.out.println("Controller.upload");
-        String message;
 
         if (ReadXlsxService.hasExcelFormat(file)) {
             try {
                 allSwipes = ReadXlsxService.readAllRows(file.getInputStream());
-                message = "Successfully uploaded: " + file.getOriginalFilename();
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(new ResponseMessage(message));
+                return "redirect:/shift-info";
             } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
-                        .body(new ResponseMessage("Unable to readAllRows file: " + file.getName()));
+                return "failedUploadView";
             }
         } else {
-            message = "Please upload an excel file!";
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseMessage(message));
+            return "failedUploadView";
         }
     }
 
