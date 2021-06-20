8f8e5a3e0fe961c427deb2192591a4a14aab491e
Gaurav Dogra <gauru.dogra@gmail.com>
2021-06-20 21:38:17 +0100

    MyController.java, uploaded download(), officers are extracted in this method, dtoWrapper is updated with officer names and then added to the model

--- src/main/java/gmailgdogra/MyController.java
+++ src/main/java/gmailgdogra/MyController.java
@@ -32,16 +32,17 @@ public class MyController {
 
         if (ReadXlsxService.hasExcelFormat(file)) {
             try {
+                model.addAttribute("msg", "Successfully Uploaded: " + file.getOriginalFilename());
+
                 allSwipes = ReadXlsxService.readAllRows(file.getInputStream());
                 Set<Officer> officers = ExtractOfficers.from(allSwipes);
-                ShiftsWrapper shiftsWrapper = new ShiftsWrapper();
-                List<Shift> shifts = new ArrayList<>();
+                List<ShiftDto> shiftDtos = new ArrayList<>();
                 for (Officer officer : officers) {
-                    shifts.add(new Shift(officer));
+                    shiftDtos.add(new ShiftDto(officer.toString(), 0));
                 }
-                shiftsWrapper.setShifts(shifts);
-                model.addAttribute("shiftsWrapper", shiftsWrapper);
-                model.addAttribute("msg", "Successfully Uploaded: " + file.getOriginalFilename());
+                DtoWrapper dtoWrapper = new DtoWrapper();
+                dtoWrapper.setShifts(shiftDtos);
+                model.addAttribute("dtoWrapper", dtoWrapper);
                 return "shift-info";
             } catch (Exception e) {
                 model.addAttribute("msg", "Uploaded file is not in expected format");
@@ -53,29 +54,47 @@ public class MyController {
     }
 
     @GetMapping("/download")
-    public ResponseEntity<ByteArrayResource> download() {
+    public ResponseEntity<ByteArrayResource> download(Model model) {
         System.out.println("Controller.download");
         try {
-            if (allSwipes == null) {
-                throw new Exception();
-            }
             String filename = createFileName();
             HttpHeaders header = new HttpHeaders();
             header.setContentType(new MediaType("application", "force-download"));
             header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename);
-            Set<Officer> officers = ExtractOfficers.from(allSwipes);
-            List<Shift> shifts = UserInput.getShiftDetails(officers);
+            List<Shift> shifts = getShiftsDetails();
             List<OutputRow> outputData = SwipeProcessor.getOutputDataFrom(allSwipes, shifts);
             ByteArrayResource resource = new ByteArrayResource(WriteOutputToXlsx.write(outputData));
             return new ResponseEntity<>(resource, header, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
         }
-
     }
 
     private String createFileName() {
         String date = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         return String.format("Securitas Report %s.xlsx", date);
+    }
+
+    private List<Shift> getShiftsDetails() {
+        Officer anthony = new Officer("Anthony", "Capes");
+        Officer gaurav = new Officer("GAURAV", "DOGRA");
+        Officer zubair = new Officer("Zubair", "Patel");
+        Officer dean = new Officer("Dean", "Colquhoun");
+        Officer omo = new Officer("Omoogbolahan", "Adeola");
+        Officer derek = new Officer("Derek", "Devlin");
+        Officer mark = new Officer("Mark", "Seth");
+        Officer reehad = new Officer("Reehad", "Ali");
+        Officer kayode = new Officer("Kayode", "Dairo");
+        return List.of(
+                new Shift(anthony, Location.TL_PLAISTOW, false),
+                new Shift(gaurav, Location.MAIN_GATE, false),
+                new Shift(zubair, Location.VISITORS_RECEPTION, true),
+                new Shift(dean, Location.MAIN_GATE, true),
+                new Shift(omo, Location.EP_WEIGHBRIDGE, false),
+                new Shift(derek, Location.TL_PLAISTOW, true),
+                new Shift(mark, Location.MAIN_GATE, true),
+                new Shift(reehad, Location.EP_WEIGHBRIDGE, true),
+                new Shift(kayode, Location.MAIN_GATE, false)
+        );
     }
 }
