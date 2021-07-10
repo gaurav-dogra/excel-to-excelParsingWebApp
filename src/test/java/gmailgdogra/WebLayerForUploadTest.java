146424c3f068147a1662490ddf2d8e04439ebde9
gauru.dogra@gmail.com <gauru.dogra@gmail.com>
2021-07-10 03:38:31 +0100

    removed unit test from WebLayerForUploadTest.java

--- src/test/java/gmailgdogra/WebLayerForUploadTest.java
+++ src/test/java/gmailgdogra/WebLayerForUploadTest.java
@@ -1,36 +1,7 @@
 package gmailgdogra;
 
-import org.apache.commons.compress.utils.IOUtils;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.io.File;
-import java.io.FileInputStream;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class WebLayerForUploadTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    void shouldReturnStatusOk() throws Exception {
-        File file = new File("src/main/resources/testFile.xlsx");
-        FileInputStream inputStream = new FileInputStream(file);
-        MockMultipartFile multipartFile = new MockMultipartFile("file",
-                file.getName(),
-                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                IOUtils.toByteArray(inputStream));
-
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/upload")
-                .file(multipartFile))
-                .andExpect(status().isOk());
-    }
-
 }
