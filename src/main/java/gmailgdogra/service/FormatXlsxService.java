6f572a1c52ebc32494efd891c45e659ad0fe128f
gauru.dogra@gmail.com <gauru.dogra@gmail.com>
2021-07-11 23:42:21 +0100

    Writing simple data to xlsx file and file formatting split to two classes

---
+++ src/main/java/gmailgdogra/service/FormatXlsxService.java
@@ -0,0 +1,145 @@
+package gmailgdogra.service;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.time.LocalTime;
+
+import static gmailgdogra.AppConstants.*;
+
+public class FormatXlsxService {
+
+    private static XSSFWorkbook workbook;
+    private static XSSFSheet sheet;
+
+    public static XSSFWorkbook of(XSSFWorkbook plainXlsx) {
+        workbook = plainXlsx;
+        sheet = workbook.getSheetAt(0);
+        insertEmptyRowAfterEachLocation();
+        applyStyles();
+        return workbook;
+    }
+
+    private static void insertEmptyRowAfterEachLocation() {
+        for (int rowNo = 2; rowNo < sheet.getPhysicalNumberOfRows(); rowNo++) {
+            String previousRowLocation = sheet.getRow(rowNo - 1).getCell(0).getStringCellValue();
+            String currentRowLocation = sheet.getRow(rowNo).getCell(0).getStringCellValue();
+            if (!previousRowLocation.equals(currentRowLocation)) {
+                insertEmptyRowBefore(rowNo);
+                rowNo++;
+            }
+        }
+    }
+
+    private static void insertEmptyRowBefore(int rowNo) {
+        shiftRowsDownByOneFrom(rowNo);
+        Row row = sheet.createRow(rowNo);
+        for (int col = LOCATION_COL_NO; col <= LOGICAL_DEVICE_COL_NO; col++) {
+            row.createCell(col).setBlank();
+        }
+    }
+
+    private static void shiftRowsDownByOneFrom(int rowNo) {
+        sheet.shiftRows(rowNo, sheet.getLastRowNum(), 1);
+    }
+
+    private static void applyStyles() {
+        XSSFCellStyle titleRowStyle = createTitleRowStyle();
+        XSSFCellStyle highlightedRowStyle = createHighlightedRowStyle();
+        XSSFCellStyle normalRowStyle = createNormalRowStyle();
+
+        for (Row row : sheet) {
+
+            int currentRowNo = row.getRowNum();
+
+            if (currentRowNo == TITLE_ROW_NO) {
+                applyStyle(currentRowNo, titleRowStyle);
+                continue;
+            }
+
+            if (rowIsEmpty(currentRowNo)) {
+                applyStyle(currentRowNo, titleRowStyle);
+                continue;
+            }
+
+            if (rowHasNoSwipe(currentRowNo) ||
+                    swipeInIsLate(currentRowNo)) {
+                applyStyle(currentRowNo, highlightedRowStyle);
+            } else {
+                applyStyle(currentRowNo, normalRowStyle);
+            }
+        }
+    }
+
+    private static boolean rowHasNoSwipe(int currentRowNo) {
+        Row row = sheet.getRow(currentRowNo);
+        String swipeInfoColVal = row.getCell(EVENT_DATE_COL_NO).getStringCellValue();
+        return "No Swipe Found".equals(swipeInfoColVal);
+    }
+
+    private static boolean rowIsEmpty(int currentRowNo) {
+        Row row = sheet.getRow(currentRowNo);
+        String firstNameColVal = row.getCell(FIRST_NAME_COL_NO).getStringCellValue();
+        return "".equals(firstNameColVal);
+    }
+
+    private static boolean swipeInIsLate(int currentRowNo) {
+        Row row = sheet.getRow(currentRowNo);
+        String locationCellVal = row.getCell(LOCATION_COL_NO).getStringCellValue();
+        String timeInfo = row.getCell(EVENT_DATE_COL_NO).getStringCellValue().substring(TIME_INFO_START_INDEX);
+        String logicalDevCellVal = row.getCell(LOGICAL_DEVICE_COL_NO).getStringCellValue();
+        LocalTime swipeInTime = LocalTime.parse(timeInfo);
+
+        if (logicalDevCellVal.endsWith("OUT")) {
+            return false;
+        } else if (swipeInTime.isAfter(LocalTime.parse("04:00")) && swipeInTime.isBefore(LocalTime.parse("06:01"))) {
+            return false;
+        } else if (swipeInTime.isAfter(LocalTime.parse("16:00")) && swipeInTime.isBefore(LocalTime.parse("18:01"))) {
+            return false;
+        } else return !"Visitors Reception".equals(locationCellVal) || !swipeInTime.isBefore(LocalTime.parse("07:01"));
+    }
+
+
+    private static XSSFCellStyle createTitleRowStyle() {
+        XSSFCellStyle style = workbook.createCellStyle();
+        style.setFillForegroundColor(IndexedColors.YELLOW.index);
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        Font font = workbook.createFont();
+        font.setBold(true);
+        style.setFont(font);
+        setThinBorderAllSides(style);
+        return style;
+    }
+
+    private static void setThinBorderAllSides(XSSFCellStyle style) {
+        style.setBorderTop(BorderStyle.THIN);
+        style.setBorderBottom(BorderStyle.THIN);
+        style.setBorderLeft(BorderStyle.THIN);
+        style.setBorderRight(BorderStyle.THIN);
+    }
+
+    private static XSSFCellStyle createHighlightedRowStyle() {
+        XSSFCellStyle style = workbook.createCellStyle();
+        Font font = workbook.createFont();
+        font.setColor(IndexedColors.DARK_RED.index);
+        style.setFont(font);
+        setThinBorderAllSides(style);
+        return style;
+    }
+
+    private static XSSFCellStyle createNormalRowStyle() {
+        XSSFCellStyle style = workbook.createCellStyle();
+        setThinBorderAllSides(style);
+        return style;
+    }
+
+    private static void applyStyle(int rowNo, XSSFCellStyle titleRowStyle) {
+        Sheet sheet = workbook.getSheetAt(0);
+        Row row = sheet.getRow(rowNo);
+        for (int i = 0; i < 5; i++) {
+            row.getCell(i).setCellStyle(titleRowStyle);
+        }
+    }
+}
---
+++ src/main/java/gmailgdogra/service/WriteToXlsxService.java
@@ -0,0 +1,58 @@
+package gmailgdogra.service;
+
+import gmailgdogra.pojo.OutputRow;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.util.List;
+
+import static gmailgdogra.AppConstants.*;
+
+public class WriteToXlsxService {
+
+    private static final OutputRow titleRow = OutputRow.of("Location", "First Name",
+            "Last Name", "Event Date", "Logical Device");
+
+    private static int rowsCreatedSoFar = 0;
+
+    public static XSSFWorkbook write(List<OutputRow> outputData) {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Securitas Daily Report");
+        writeRow(sheet, titleRow);
+        writeDataRows(sheet, outputData);
+        autoSizeColumns(sheet);
+        return workbook;
+    }
+
+    private static void writeDataRows(Sheet sheet, List<OutputRow> outputData) {
+        for (OutputRow outputDatum : outputData) {
+            writeRow(sheet, outputDatum);
+        }
+    }
+
+    private static Row getNewRow(Sheet sheet) {
+        return sheet.createRow(rowsCreatedSoFar++);
+    }
+
+    private static void writeRow(Sheet sheet, OutputRow rowData) {
+        Row row = getNewRow(sheet);
+
+        for (int col = 0; col < 5; col++) {
+            row.createCell(col);
+        }
+
+        row.getCell(LOCATION_COL_NO).setCellValue(rowData.getLocation());
+        row.getCell(FIRST_NAME_COL_NO).setCellValue(rowData.getFirstName());
+        row.getCell(LAST_NAME_COL_NO).setCellValue(rowData.getLastName());
+        String swipeEvent = rowData.getEventDate() == null ? "No Swipe Found" : rowData.getEventDate();
+        row.getCell(EVENT_DATE_COL_NO).setCellValue(swipeEvent);
+        row.getCell(LOGICAL_DEVICE_COL_NO).setCellValue(rowData.getLogicalDevice());
+    }
+
+    private static void autoSizeColumns(Sheet sheet) {
+        for (int column = 0; column < 5; column++) {
+            sheet.autoSizeColumn(column);
+        }
+    }
+}
