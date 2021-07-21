385708ac7132488b089f1617dd4275508c309fdb
gauru.dogra@gmail.com <gauru.dogra@gmail.com>
2021-07-21 01:01:26 +0100

    a name change of a constant

--- src/main/java/gmailgdogra/AppConstants.java
+++ src/main/java/gmailgdogra/AppConstants.java
@@ -12,12 +12,11 @@ public class AppConstants {
     public static final int TITLE_ROW_NO = 0;
 
     public static final int SHIFT_DATE_ROW = 3;
-    public static final int DATA_ROW = 7;
+    public static final int DATA_ROWS_START = 7;
     public static final int FULL_NAME_COL = 1;
     public static final int TIME_ON_COL = 2;
     public static final int TIME_OFF_COL = 3;
     public static final int HOURS_COMPLETED_COL = 4;
-
 
     private AppConstants() {}
 
