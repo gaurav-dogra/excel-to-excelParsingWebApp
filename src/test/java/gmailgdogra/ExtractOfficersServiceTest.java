bf21c95d52e1fb3d215e6db073d294e904d36959
gauru.dogra@gmail.com <gauru.dogra@gmail.com>
2021-08-06 04:09:45 +0100

    Updated ExtractOfficerServiceTest, removed other dependencies when testing the only method

--- src/test/java/gmailgdogra/ExtractOfficersServiceTest.java
+++ src/test/java/gmailgdogra/ExtractOfficersServiceTest.java
@@ -3,60 +3,34 @@ package gmailgdogra;
 import gmailgdogra.pojo.Officer;
 import gmailgdogra.pojo.SwipeRecord;
 import gmailgdogra.service.ExtractOfficersService;
-import gmailgdogra.service.ReadXlsxService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.FileInputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
 class ExtractOfficersServiceTest {
 
-    private final ReadXlsxService readXlsxService;
+    private final Officer officerOne = new Officer("Officer", "One");
+    private final Officer officerTwo = new Officer("Officer", "Two");
 
-    @Autowired
-    public ExtractOfficersServiceTest(ReadXlsxService readXlsxService) {
-        this.readXlsxService = readXlsxService;
-    }
-
-    private final Set<Officer> expectedOfficers = new HashSet<>(Arrays.asList(
-            new Officer("Officer", "One"),
-            new Officer("Officer", "Two"),
-            new Officer("Officer", "Three"),
-            new Officer("Officer", "Four"),
-            new Officer("Officer", "Five"),
-            new Officer("Officer", "Six"),
-            new Officer("Officer", "Seven"),
-            new Officer("Officer", "Eight"),
-            new Officer("Officer", "Nine"),
-            new Officer("Officer", "Ten"),
-            new Officer("Officer", "Eleven"),
-            new Officer("Officer", "Twelve")
+    private final Set<Officer> expected = new HashSet<>(Arrays.asList(
+            officerOne,
+            officerTwo
     ));
 
+    private final List<SwipeRecord> inputList = Arrays.asList(
+            new SwipeRecord(officerOne, null, null),
+            new SwipeRecord(officerOne, null, null),
+            new SwipeRecord(officerTwo, null, null)
+    );
+
     @Test
-    void test_from() {
-        Path path = Paths.get("src/main/resources/test file 1.xlsx");
-        Set<Officer> officers = null;
-
-        try {
-            List<SwipeRecord> swipeRecords = readXlsxService.readAllRows(new FileInputStream(path.toFile()));
-            officers = ExtractOfficersService.from(swipeRecords);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            Assertions.fail("Unable to read file at " + path);
-        }
-
-        assertTrue(officers.containsAll(expectedOfficers));
+    public void test_distinctReturn() {
+        assertEquals(expected, ExtractOfficersService.from(inputList));
     }
+
 }
